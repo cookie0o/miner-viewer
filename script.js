814823f4afd8f0e167b2071abc3f772c29248f3b
cookie0_o <81589649+cookie0o@users.noreply.github.com>
2023-08-03 01:48:18 +0200

    Improved Phone view

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 if (localStorage.getItem("monerokey") === null) {window.open("settings/", "_self")} else {
 key = localStorage.getItem("monerokey");}
 
-
 _SelectedCurrency = localStorage.getItem("currency");
 
 if (localStorage.getItem("moneroStorage") !== null) {
@@ -29,9 +28,9 @@ function getCurrencySymbol(currencyCode) {
 // format hash rate for a better readability
 function formatHashrate(hashrate) {
     if (hashrate >= 1000000) {
-      return (hashrate / 1000000).toFixed(2) + ' M/s';
+      return (hashrate / 1000000).toFixed(2) + ' MH/s';
     } else if (hashrate >= 1000) {
-      return (hashrate / 1000).toFixed(2) + ' K/s';
+      return (hashrate / 1000).toFixed(2) + ' KH/s';
     } else {
       return hashrate.toFixed(2) + ' H/s';
     }
@@ -53,6 +52,7 @@ setInterval(async () => {
     existingData = renderGraphs(walletDetails, existingData);
     renderRigs(walletDetails);
 }, 5000);
+
 
 function GetXMR_Currency_value(currentBalanceXMR) {
     const selectedCurrency = SelectedCurrency(_SelectedCurrency);
@@ -139,84 +139,6 @@ function GetXMR_Currency_value(currentBalanceXMR) {
 // average num calc values
 let sum = 0;
 let count = 0;
-
-function GetXMR_Currency_value(currentBalanceXMR) {
-    const selectedCurrency = SelectedCurrency(_SelectedCurrency);
-    const apiEndpoint = `https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=${selectedCurrency}`;
-
-    // Function to fetch the current Monero price from the API
-    async function fetchMoneroPrice() {
-    try {
-        const response = await fetch(apiEndpoint);
-        const data = await response.json();
-        return data.monero[selectedCurrency];
-    } catch (error) {
-        console.error('Error fetching Monero price:', error);
-        return null;
-    }
-    }
-
-    // Function to get the current timestamp
-    function getCurrentTimestamp() {
-        return Math.floor(Date.now() / 1000);
-    }
-
-    // Function to get the stored price and timestamp from localStorage
-    function getStoredPrice() {
-        const storedPrice = localStorage.getItem('moneroPrice');
-        const storedTimestamp = localStorage.getItem('moneroTimestamp');
-        return { price: storedPrice, timestamp: parseInt(storedTimestamp) };
-    }
-
-    // Function to set the price and timestamp in localStorage
-    function setStoredPrice(price, timestamp) {
-        localStorage.setItem('moneroPrice', price);
-        localStorage.setItem('moneroTimestamp', timestamp);
-    }
-
-    // Function to update the Monero price every 20 seconds
-    async function updateMoneroPrice() {
-        const storedData = getStoredPrice();
-        const currentTime = getCurrentTimestamp();
-
-        // Check if the stored price is less than 20 seconds old
-        if (storedData.price && currentTime - storedData.timestamp < 20) {
-            // Use the stored value
-            return storedData.price;
-        } else {
-            // Fetch the new price from the API
-            const newPrice = await fetchMoneroPrice();
-            if (newPrice) {
-                setStoredPrice(newPrice, currentTime);
-                console.log('Updated Monero price to:', newPrice);
-                return newPrice;
-            } else {
-                // If there's an error fetching the new price, use the stored value
-                console.log('Fetching Monero price failed with error:', JSON.stringify(error));
-                return storedData.price;
-            }
-    }
-    }
-
-    return new Promise((resolve, reject) => {
-    updateMoneroPrice()
-        .then((xmrToCurrencyRate) => {
-        // Calculate the equivalent value in the selected currency
-        const currentBalance = currentBalanceXMR * xmrToCurrencyRate;
-
-        // check if resp. is a number or not
-        if (xmrToCurrencyRate === undefined) {
-            resolve('N/A');
-        } else {
-            resolve(currentBalance.toFixed(2));
-        }
-        })
-        .catch((error) => {
-        // Reject the promise with the error
-        reject(error);
-        });
-    });
-}
 
 function renderGraphs(walletDetails, existingData) {
     const xmrAmountGraph = document.getElementById('xmr-amount');
@@ -393,7 +315,11 @@ function renderRigs(walletDetails) {
         }
 
         if (active) {
-            $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="big">${hashrate}/s</p></div><div class="collumn" id="hashes"><p class="big">${walletDetails.perWorkerStats[i].hashes}</p></div><div class="collumn"><p class="big">${expiredHashes}</p></div><div class="collumn"><p class="big">${invalidHashes}</p></div><div class="collumn"><p class="big">${formatLastShareDate(walletDetails.perWorkerStats[i].lastShare)}</p></div></div></div>`);
+            if (navigator.userAgentData.mobile == true) {
+                $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="big">${hashrate}/s</p></div><div class="collumn"><p class="big">${formatLastShareDate(walletDetails.perWorkerStats[i].lastShare)}</p></div></div></div>`);
+            } else {
+                $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="big">${hashrate}/s</p></div><div class="collumn" id="hashes"><p class="big">${walletDetails.perWorkerStats[i].hashes}</p></div><div class="collumn"><p class="big">${expiredHashes}</p></div><div class="collumn"><p class="big">${invalidHashes}</p></div><div class="collumn"><p class="big">${formatLastShareDate(walletDetails.perWorkerStats[i].lastShare)}</p></div></div></div>`);
+            }
         }
         // $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="small">Hashrate</p><p class="big">${hashrate}/s</p></div><div class="collumn" id="hashes"><p class="small">Accepted Hashes</p><p class="big">${walletDetails.perWorkerStats[i].hashes}</p></div><div class="collumn" id="expired"><p class="small">Expired Hashes</p><p class="big">0</p></div><div class="collumn" id="invalid"><p class="small">Invalid Hashes</p><p class="big">0</p></div></div><div class="collumn right" id="lastshare"><p class="small">Last Share</p><p class="big">Today</p></div></div>`);
     }
@@ -418,8 +344,30 @@ function renderRigs(walletDetails) {
         }
 
         if (!active) {
-            $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="big">${hashrate}/s</p></div><div class="collumn" id="hashes"><p class="big">${walletDetails.perWorkerStats[i].hashes}</p></div><div class="collumn"><p class="big">${expiredHashes}</p></div><div class="collumn"><p class="big">${invalidHashes}</p></div><div class="collumn"><p class="big">${formatLastShareDate(walletDetails.perWorkerStats[i].lastShare)}</p></div></div></div>`);
+            if (navigator.userAgentData.mobile == true) {
+                $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="big">${hashrate}/s</p></div><div class="collumn"><p class="big">${formatLastShareDate(walletDetails.perWorkerStats[i].lastShare)}</p></div></div></div>`);
+            } else {
+                $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="big">${hashrate}/s</p></div><div class="collumn" id="hashes"><p class="big">${walletDetails.perWorkerStats[i].hashes}</p></div><div class="collumn"><p class="big">${expiredHashes}</p></div><div class="collumn"><p class="big">${invalidHashes}</p></div><div class="collumn"><p class="big">${formatLastShareDate(walletDetails.perWorkerStats[i].lastShare)}</p></div></div></div>`);
+            }
         }
         // $(".rigs .rigscontainer").append(`<div class="rig ${activeClass}"><p class="name">${walletDetails.perWorkerStats[i].workerId}</p><div class="data"><div class="collumn"><p class="small">Hashrate</p><p class="big">${hashrate}/s</p></div><div class="collumn" id="hashes"><p class="small">Accepted Hashes</p><p class="big">${walletDetails.perWorkerStats[i].hashes}</p></div><div class="collumn" id="expired"><p class="small">Expired Hashes</p><p class="big">0</p></div><div class="collumn" id="invalid"><p class="small">Invalid Hashes</p><p class="big">0</p></div></div><div class="collumn right" id="lastshare"><p class="small">Last Share</p><p class="big">Today</p></div></div>`);
     }
+}
+
+// check if browser is mobile to change some css
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+
+// if the user is using a mobile device change some website elements
+if (navigator.userAgentData.mobile == true) {
+    // Select the title2 div
+    const title2Div = document.querySelector('.title2');
+
+    // Remove unwanted paragraphs based on their index (2, 3, 4)
+    const unwantedIndexes = [2, 3, 4];
+    const paragraphs = title2Div.querySelectorAll('p');
+    unwantedIndexes.forEach(index => paragraphs[index].remove());
 }
