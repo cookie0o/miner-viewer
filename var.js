e558958c94e92f46391f6c0b5ca702c2808e5c36
cookie0_o <81589649+cookie0o@users.noreply.github.com>
2023-08-06 04:38:40 +0200

    Delete var.js

--- var.js
+++
@@ -1,5 +0,0 @@
-export var StorageKey_1 = localStorage.getItem("monerokey");
-export var Key_check_1 = StorageKey.match(/4[0-9AB][123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{93}/);
-
-export const sidebar = document.getElementById('sidebar');
-export const toggleBtn = document.getElementById('navbar_btn_');
